69d65a24824639e8b83fae2224595cb4d3f303c9
Adam Leung <centribo@hotmail.com>
2016-03-12 16:57:33 -0500

    Sound effects

--- escape/Assets/Scripts/MazeButtonController.cs
+++ escape/Assets/Scripts/MazeButtonController.cs
@@ -24,6 +24,7 @@ public class MazeButtonController : MonoBehaviour {
 	public void ClickButton(){
 		if(TileManager.Instance.IsFinishedMaze()){
 			GameManager.Instance.SetSolved(solvedRoom, true);
+			GetComponent<CardboardAudioSource>().Play();
 		}
 	}
 
