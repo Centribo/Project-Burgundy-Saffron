b1aa011c713943c6663cd4e0fd13d1682c959d1b
Adam Leung <centribo@hotmail.com>
2016-03-13 05:18:13 -0400

    Started work on control room, finished all 3 puzzle hints, and started work on keypad

---
+++ escape/Assets/Scripts/EndGameButtonController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGameButtonController : MonoBehaviour {
+
+	public bool isGazedAt = false;
+
+	Color originalColor;
+
+	// Use this for initialization
+	void Start () {
+		originalColor = GetComponent<Renderer>().material.color;
+		UpdateVisuals();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetGazedAt(bool gaze){
+		isGazedAt = gaze;
+		UpdateVisuals();
+	}
+
+	public void ClickButton(){
+
+	}
+
+	public void UpdateVisuals(){
+		GetComponent<Renderer>().material.color = isGazedAt ? new Color(0.1f, 0.1f, 0.1f) : originalColor;
+	}
+
+}
---
+++ escape/Assets/Scripts/PasscodeController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PasscodeController : MonoBehaviour {
+
+	public static PasscodeController instance = null;
+	public static PasscodeController Instance { //Singleton pattern instance
+		get { //Getter
+			if(instance == null){ //If its null,
+				instance = (PasscodeController)FindObjectOfType(typeof(PasscodeController)); //Find it
+			}
+			return instance; //Return it
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void EnterDigit(int digit){
+
+	}
+
+	public void TryPasscode(){
+
+	}
+}
